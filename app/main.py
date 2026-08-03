5b9b873b009db677fffe069dbb0b8c79ecc6c03c
Niranjan Sharma <niranjansharma78@gmail.com>
2026-08-03 21:48:15 +0530

    Fix Dashboard V2 template rendering

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.api.routes import router
+from app.api.dashboard_intelligence_routes import router as dashboard_intelligence_router
 from app.api.developer_console_routes import router as developer_console_router
 from app.api.imap_routes import router as imap_router
 from app.core.database import init_db
@@ -13,6 +14,7 @@ app = FastAPI(title="Nexus DP1", version="0.1.0")
 app.mount("/static", StaticFiles(directory=BASE/"static"), name="static")
 templates = Jinja2Templates(directory=BASE/"templates")
 app.include_router(router)
+app.include_router(dashboard_intelligence_router)
 app.include_router(developer_console_router)
 app.include_router(imap_router)
 
@@ -22,8 +24,16 @@ def startup():
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={},
+    )
 
 @app.get("/developer", response_class=HTMLResponse)
 def developer(request: Request):
-    return templates.TemplateResponse("developer.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="developer.html",
+        context={},
+    )
